2c4a728d34a4e0bd16fd9381b427d58fad96097f
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-04-02 20:25:33 +0530

    added componets and customHooks

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) => {
+  return (
+    <div className={`${className}`}>
+      <div className="w-1-2">
+        <label className=" text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          type="number"
+          className=" outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) => {
+            onAmountChange && onAmountChange(Number(e.target.value));
+          }}
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className=" rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectedCurrency}
+          disabled={currencyDisabled}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map((currency) => {
+            <option key={currency} value={currency}>
+              {currency}
+            </option>;
+          })}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default InputBox;
---
+++ src/customHook/useCurrencyInfo.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const useCurrencyInfo = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(``);
+  }, []);
+  return <div>useCurrencyInfo</div>;
+};
+
+export default useCurrencyInfo;
